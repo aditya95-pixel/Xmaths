cff654a05cddaa803e27b428df927f7bd8866634
aditya95-pixel <sphs.am6@gmail.com>
2026-03-12 01:13:07 +0530

    front-end cleanup

--- components/Message.jsx
+++ components/Message.jsx
@@ -19,9 +19,9 @@ const Message = ({role,content}) => {
         toast.success("Message copied to clipboard")
     }
   return (
-    <div className='flex flex-col items-center w-full max-w-3xl text-base'>
+    <div className='flex flex-col items-center w-full max-w-6xl text-base'>
         <div className={`flex flex-col w-full mb-8 ${role==='user' && 'items-end'}`}>
-            <div className={`group relative flex max-w-2xl py-3 rounded-xl
+            <div className={`group relative flex max-w-full py-3 rounded-xl
                 ${role==='user' ? 'dark:bg-[#414158] bg-gray-300 px-5' : 'gap-3'}`}>
                     <div className={`opacity-0 group-hover:opacity-100 absolute
                     ${role=='user' ? '-left-16 top-2.5' : 'left-9 -bottom-6'} transition-all`}>
