4a156579a763db13a7ff162fed7849fa8de764db
aditya95-pixel <sphs.am6@gmail.com>
2026-03-18 21:12:24 +0530

    added learning path page

---
+++ app/learning_path/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LearningPath = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-10">
+      <h1 className="text-3xl font-bold">Learning Path</h1>
+      <p className="mt-4 text-gray-500">Welcome to your new explore page!</p>
+    </div>
+  )
+}
+
+export default LearningPath
